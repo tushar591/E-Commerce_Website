258d8446e02cb0010f309b52bf38ee3ef3764bdd
TUSHAR <tushar9445958996@gmail.com>
2025-01-16 14:42:44 +0530

    Added

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 dotenv.config();
 
 const port = process.env.port || 3000;
+const DB_URI = process.env.MONGO_URI;
 
+try {
+  await mongoose.connect(DB_URI);
+  console.log("Connected");
+} catch (error) {
+  console.log("Error Connecting to DB");
+}
 
 
 app.listen(port, () => {
