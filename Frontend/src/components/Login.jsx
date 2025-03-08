d76a3890ace79aba912770467f48caac2fc25227
TUSHAR <tushar9445958996@gmail.com>
2025-03-08 18:17:30 +0530

    started login and signup

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import logo from "../../public/wp5231557.jpg";
 
-export default function Login() {
+function Login() {
+
+  const [email, setEmail] = useState(""); 
+  const [password, setPassword] = useState(""); 
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    console.log("entered submit");
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:4002/api/v1/user/login", {
+        email,
+        password,
+      },{
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+      console.log(response);
+      navigate("/");
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
   return (
-    <div>
-      Login
+    <div className="bg-gradient-to-r from-black to-blue-950 ">
+      <div className="h-screen container mx-auto flex  items-center justify-center text-white">
+
+        {/* Login Form */}
+        <div className="bg-gray-900 p-8 rounded-lg shadow-lg w-[500px] m-8 md:m-0 mt-20">
+          <h2 className="text-2xl font-bold mb-4 text-center">
+            Welcome to <span className="text-orange-500">CourseHaven</span>
+          </h2>
+          <p className="text-center text-gray-400 mb-6">
+            Log in to access paid content!
+          </p>
+
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label htmlFor="email" className=" text-gray-400 mb-2">
+                Email
+              </label>
+              <input
+                type="text"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full p-3 rounded-md bg-gray-800 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="name@email.com"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="password" className=" text-gray-400 mb-2">
+                Password
+              </label>
+              <div className="relative">
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full p-3 rounded-md bg-gray-800 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="********"
+                  required
+                />
+                <span className="absolute right-3 top-3 text-gray-500 cursor-pointer">
+                  👁️
+                </span>
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-orange-500 hover:bg-blue-600 text-white py-3 px-6 rounded-md transition"
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
+export default Login;
